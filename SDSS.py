b81108a36f8ba161dc694e4dcb97091783680622
nan <nsahu@swin.edu.au>
2018-11-20 16:52:20 +1100

    reading 19th fits file

---
+++ SDSS.py
@@ -0,0 +1,13 @@
+#import SDSSdata
+from astropy.io import fits
+
+filename="spec-10000-57346-0019.fits"
+
+hdu_list=fits.open(filename)
+
+print(len(hdu_list))
+print(hdu_list[0].header)
+#print(hdu_list.info())
+
+for key, value in hdu_list[0].header.items():
+ print("{0}={1}".format(key,value))
