c078089be4eea9df7254594279128512c826a5a8
nan <nsahu@swin.edu.au>
2018-11-23 13:50:59 +1100

    With regular expression and modified plots a little

--- SDSS.py
+++ SDSS.py
@@ -5,8 +5,8 @@ filename="spec-10000-57346-0019.fits"
 
 hdu_list=fits.open(filename)
 
-print(len(hdu_list))
-print(hdu_list[0].header)
+#print(len(hdu_list))
+#print(hdu_list[0].header)
 #print(hdu_list.info())
 
 for key, value in hdu_list[0].header.items():
--- read_SDSS.py
+++ read_SDSS.py
@@ -1,7 +1,9 @@
 from class_spectra import Spectrum
 import argparse
 import astropy
+from astropy.io import fits
 import matplotlib.pyplot as plt
+import re
 
 parser = argparse.ArgumentParser(description="This program reads a given SDSS"+
                                  " spectrum and plots it.",
@@ -11,16 +13,27 @@ parser.add_argument('-f', '--file',
                     help="File to open.")
 args = parser.parse_args()
 
+filematch=re.match("([a-zA-Z]+)\-([0-9]+)\-([0-9]+)\-([0-9]+).fits",args.file)
+assert filematch is not None, "The filename is not correct."
 
 spec1 = Spectrum(args.file)
+
 
 
 plt.figure()
 x = spec1.wavelength
 y = spec1.flux
+n = spec1.header_card('NAME')
+ra= spec1.header_card('RA')
+dec= spec1.header_card('DEC')
 
-plt.plot(x,y)
 
-plt.xlabel("Wavelength")
-plt.ylabel("Flux")
+
+plt.plot(x,y,label="NAME={},RA={},DEC={}".format(n, ra, dec))
+
+plt.xlabel("Wavelength (Angstroms)")
+plt.ylabel("Flux (erg/cm^2/s/Ang)")
+
+plt.legend(loc='upper center', fontsize=7)
+
 plt.show()
